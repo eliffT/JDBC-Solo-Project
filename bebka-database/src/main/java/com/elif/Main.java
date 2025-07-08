816bb2bdb3a92c5e0a0e8c5084520b1744b88906
eliffT <elifftasdemirr@gmail.com>
2025-07-08 23:35:51 +0300

    [JA-3] - JDBC Connection

--- bebka-database/src/main/java/com/elif/Main.java
+++ bebka-database/src/main/java/com/elif/Main.java
@@ -1,7 +1,17 @@
 package com.elif;
 
+import com.elif.config.DatabaseConnectionConfig;
+
+import java.sql.Connection;
+
 public class Main {
     public static void main(String[] args) {
-
+        try{
+            Connection conn = DatabaseConnectionConfig.getConnection();
+            System.out.println("Connected!");
+            conn.close();
+        }catch(Exception e){
+            throw new RuntimeException(e);
+        }
     }
 }
